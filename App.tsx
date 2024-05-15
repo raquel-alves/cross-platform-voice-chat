b5f4e698a28fcc5d0e8dfa4dc493eea4e56788d0
Raquel <souraquelalvez@gmail.com>
2024-05-15 17:51:34 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,14 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+import { startStream } from '@utils/streamAudio';
 
 export default function App() {
+  const [microphoneStatus, setMicrophoneStatus] = useState(false); // false for offline, true for online
+  const [play, setPlay] = useState<Function | null>(null);
+  const [stop, setStop] = useState<Function | null>(null);
+  const hasPermission = play && stop;
+
+  const toggleMicrophoneStatus = () => {
+    if (!hasPermission) return;
+    microphoneStatus ? stop() : play()
+    setMicrophoneStatus(!microphoneStatus);
+  };
+
+  useEffect(() => {
+    (async () => {
+      await startStream(({ start, end }) => {
+        setPlay(() => start);
+        setStop(() => end);
+      });
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button
+        title={microphoneStatus ? 'Mic On' : 'Mic Off'}
+        onPress={toggleMicrophoneStatus}
+        disabled={!hasPermission}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
